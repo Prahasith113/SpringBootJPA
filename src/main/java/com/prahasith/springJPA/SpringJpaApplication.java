467c3f285fe31cbcc45c8b6b8005e54911a1b099
Prahasith113 <prahasith113@gmail.com>
2026-01-05 15:31:15 +0530

    sample insertion successful

--- src/main/java/com/prahasith/springJPA/SpringJpaApplication.java
+++ src/main/java/com/prahasith/springJPA/SpringJpaApplication.java
@@ -4,12 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.prahasith.springJPA.dao.StudentRepository;
+import com.prahasith.springJPA.entity.Student;
 
 @SpringBootApplication
 public class SpringJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJpaApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringJpaApplication.class, args);
+
+		StudentRepository studentRepository = context.getBean(StudentRepository.class);
+
+		Student student = new Student();
+		student.setName("sachin");
+		student.setCity("mumbai");
+		student.setHobbie("top order batsman");
+
+		Student s1 = studentRepository.save(student);
+		System.out.println(s1);
+
 	}
 
 }

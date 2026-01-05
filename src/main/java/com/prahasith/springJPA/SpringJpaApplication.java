fe8c9648714bd0cc0b474f3211d4cf4ef56f37fc
Prahasith113 <prahasith113@gmail.com>
2026-01-05 16:19:03 +0530

    crud operations

--- src/main/java/com/prahasith/springJPA/SpringJpaApplication.java
+++ src/main/java/com/prahasith/springJPA/SpringJpaApplication.java
@@ -1,5 +1,9 @@
 package com.prahasith.springJPA;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,14 +19,60 @@ public class SpringJpaApplication {
 
 		StudentRepository studentRepository = context.getBean(StudentRepository.class);
 
-		Student student = new Student();
-		student.setName("sachin");
-		student.setCity("mumbai");
-		student.setHobbie("top order batsman");
+//		Student student = new Student();
+//		student.setName("sachin");
+//		student.setCity("mumbai");
+//		student.setHobbie("top order batsman");
+//
+//		Student s1 = studentRepository.save(student);
+//		System.out.println(s1);
+//
+//		Student s2 = new Student();
+//		s2.setName("rahul");
+//		s2.setCity("bangalore");
+//		s2.setHobbie("opening batsman");
+//		
+//		Student s3 = new Student();
+//		s3.setName("hardik");
+//		s3.setCity("mumbai");
+//		s3.setHobbie("super striker");
+//		
+//		List<Student> ls = List.of(s2, s3);
+//		
+//		List<Student> resultList =  (List<Student>) studentRepository.saveAll(ls);
+//		
+//		resultList.forEach(res->{
+//			System.out.println(res);
+//		});
 
-		Student s1 = studentRepository.save(student);
-		System.out.println(s1);
-
+//  reading data from database
+//		Optional<Student> option = studentRepository.findById(1);
+//		Student stud = option.get();
+//		System.out.println(stud);
+//
+//		Iterable<Student> students = studentRepository.findAll();
+//		Iterator<Student> itr = students.iterator();
+//		while (itr.hasNext()) {
+//			System.out.println(itr.next());
+//		}
+//		
+		
+		
+// deleting data
+//		Optional<Student> option = studentRepository.findById(2);
+//		Student stud = option.get();
+//		studentRepository.delete(stud);
+		
+//		studentRepository.deleteAll();
+		
+//update data
+		Optional<Student> option = studentRepository.findById(3);
+		Student stud = option.get();
+		stud.setName("bumrah");
+		stud.setCity("ahmedabad");
+		stud.setHobbie("pace bowler");
+		
+		studentRepository.save(stud);
+	
 	}
-
 }

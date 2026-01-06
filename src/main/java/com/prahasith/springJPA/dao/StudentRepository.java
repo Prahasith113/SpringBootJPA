c6624c3e4ca87f2b7eaab2fc559f9341b797dda5
Prahasith113 <prahasith113@gmail.com>
2026-01-06 13:34:47 +0530

    added custom methods to StudentRepository

--- src/main/java/com/prahasith/springJPA/dao/StudentRepository.java
+++ src/main/java/com/prahasith/springJPA/dao/StudentRepository.java
@@ -1,9 +1,15 @@
 package com.prahasith.springJPA.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.prahasith.springJPA.entity.Student;
 
 public interface StudentRepository extends CrudRepository<Student, Integer> {
 	
+	// custom methods these are automatically implemented by JPA as per our entity values;
+	public List<Student> findByName(String name);
+	public List<Student> findByCity(String city);
+	public List<Student> findByHobbie(String hobbie);
 }
